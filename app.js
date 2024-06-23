5d97c22a8b38430227220ff2f75ef6d8e1d23b64
imranNBZ <135537322+imranNBZ@users.noreply.github.com>
2024-06-23 00:16:45 -0400

    let and const

---
+++ app.js
@@ -0,0 +1,21 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+const PI = 3.14;
+
+
+// What is the difference between var and let?
+// answer: let is a block scope where var is not, and also you can redeclair var bit not let.
+
+
+//What is the difference between var and const? 
+// answer: same with const you cannot redclair const and it is block scope but all of the above doesnot apply to var.
+
+
+//What is the difference between let and const?
+//answer: you can reassign let but with const you can only change the value of const by methods.
+
+
+//What is hoisting?
+//answer: when redclairing a varible again with same name using var is called hoisted
